37d82f5a49823b51b635b62269a83493bd785b2c
Nicholai L'Esperance <nicholai.lesperance@gmail.com>
2020-01-02 13:25:09 -0500

    adding characters

--- bot.py
+++ bot.py
@@ -1,13 +1,17 @@
+import asyncio
 import re
-
-from disc_info import TOKEN, GUILD
 import discord
 from random import randint
 
-print(TOKEN)
+from disc_info import TOKEN, GUILD, OWNER
+from character import Character, Rolodex, SKILL, ATTR
 
 
 class DDClient(discord.Client):
+
+    def __init__(self):
+        super().__init__()
+        self.rd = None
 
     async def on_ready(self):
         if len(client.guilds) != 1:
@@ -16,6 +20,9 @@ class DDClient(discord.Client):
     
         print(f'{client.user} has connected to Discord')
         print(f'Connected to {client.guilds[0]}')
+
+        self.rd = Rolodex.load()
+        print(f'Loaded {len(self.rd)} characters')
 
     async def on_message(self, message):
         if message.author == client.user:
@@ -31,11 +38,10 @@ class DDClient(discord.Client):
             if len(args) == 0:
                 await message.channel.send(f"1d20:  **{roll()[0]}**")
             elif args[0] in ('adv', 'dis'):
-                rolls = roll(n=2)
                 if args[0] == 'adv':
-                    await message.channel.send(f"2d20+Adv:   **{max(rolls)}**   *({rolls[0]}, {rolls[1]})*")
+                    await message.channel.send(roll_d20_adv)
                 else: 
-                    await message.channel.send(f"2d20+Dis:   **{min(rolls)}**   *({rolls[0]}, {rolls[1]})*")
+                    await message.channel.send(roll_d20_dis)
             else:
                 if 'd' in args[0]:
                     n, d = args[0].split('d')
@@ -53,11 +59,80 @@ class DDClient(discord.Client):
                 rolls = roll(d, n)
                 await message.channel.send(f'{n}d{d}:   **{sum(rolls)}**   *({", ".join(str(val) for val in rolls)})*')
 
+        elif cmd == '!character':
+            if len(args) == 0:
+                await message.channel.send(f'You must provide arguments: new update')
+                return
+            if args[0] == 'new':
+                try:
+                    newchar = Character(*args[1:])
+                    self.rd.add_character([message.author.name, OWNER], newchar)
+                    self.rd.store()
+                except Exception as e:
+                    await message.channel.send(f'failed to create characted: {e}')
+
+        elif cmd == '!list':
+            await message.channel.send(f'{", ".join(self.rd.characters)}')
+
+        elif cmd.lower() in [f'!{name.lower()}' for name in self.rd.characters]:
+            charname = cmd[1:]
+            try:
+                character = self.rd.get_character(message.author.name, charname)
+                print(type(character))
+                if len(args) == 0:
+                    await message.channel.send(str(character))
+                elif args[0] == 'roll':
+                    
+                    if len(args) == 1:
+                        await message.channel.send(roll()[0])
+
+                    elif args[1].lower() in SKILL or args[1].lower() in ATTR:
+                        mod = character.get_modifier(args[1])
+                        total = roll()[0] + mod
+                        await message.channel.send(f'**{total}**  *{charname.title()} rolled {args[1].lower()}.*')
+
+                elif args[0] == 'update':
+                    
+                    if len(args) == 1:
+                        await message.channel.send(f'What am I supposed to update?')
+
+                    elif args[1] in ATTR:
+                        if len(args) == 2:
+                            await message.channel.send(f"What should I update {attr[1]} to?")
+                        else:
+                            try:
+                                newval = int(args[2])
+                                if 0 > newval > 20:
+                                    await message.channel.send("Attribute must be between 0 and 20, inclusive.")
+                                setattr(character, args[1], newval)
+                                self.rd.store()
+                                await message.channel.send(f"Updated {charname.title()}'s {args[1]} to {newval}")
+                            except ValueError:
+                                await message.channel.send("Attribute value must be an integer")
+
+
+
+            except PermissionError:
+                await message.channel.send(f'You do not have permission to use {charname.title()}')
+
+        else:
+            await message.channel.send("There is no command or character by this name")
+
+
 def roll(d=20, n=1):
     d = int(d)
     n = int(n)
     results = [randint(1, d) for die in range(n)]
     return results
+
+def roll_d20_adv():
+    rolls = roll(n=2)
+    return f"2d20+Adv:   **{max(rolls)}**   *({rolls[0]}, {rolls[1]})*"
+
+def roll_d20_dis():
+    rolls = roll(n=2)
+    return f"2d20+Dis:   **{min(rolls)}**   *({rolls[0]}, {rolls[1]})*"
+    
 
 
 client = DDClient()
---
+++ character.py
@@ -0,0 +1,131 @@
+from concurrent.futures import ProcessPoolExecutor
+from typing import Set, Dict, Tuple, Union, List
+from dataclasses import dataclass, field, asdict
+
+ATTR = {"strength", "dexterity", "intelligence", "wisdom", "charisma", "constitution"}
+
+SKILL = {"athletics": "strength",
+         "acrobatics": "dexterity",
+         "sleight_of_hand": "dexterity",
+         "arcana": "intelligence",
+         "history": "intelligence",
+         "investigation": "intelligence",
+         "nature": "intelligence",
+         "religion": "intelligence",
+         "animal_handling": "wisdom",
+         "insignt": "wisdom",
+         "medicine": "wisdom",
+         "perception": "wisdom",
+         "survival": "wisdom",
+         "deception": "charisma",
+         "intimidation": "charisma",
+         "performance": "charisma",
+         "persuasion": "charisma"
+        }    
+
+@dataclass
+class Class():
+    name: str
+    level: int
+        
+    # disallows duplicate classes in a set
+    def __hash__(self):
+        return hash(self.name)
+
+@dataclass
+class Character():
+    name: str
+    race: str
+    strength: int
+    dexterity: int
+    constitution: int
+    intelligence: int
+    wisdom: int
+    charisma: int
+    proficiencies: Set[str] = field(default_factory=set)
+    skill_modifiers: Dict[str, Tuple[str, int]] = field(default_factory=dict)
+    classes: Set[Class] = field(default_factory=set)
+        
+    def __eq__(self, other):
+        if isinstance(other, Character):
+            return (self.name == other.name)
+        return False
+        
+    @property
+    def level(self):
+        total_level = sum(cls.level for cls in self.classes)
+        return total_level or 1
+    
+    @property
+    def proficiency(self):
+        return (self.level + 3) // 4 + 1
+    
+    def add_proficiency(self, skill: str):
+        skill = skill.lower()
+        if skill not in SKILL:
+            raise ValueError('invalid skillname')
+        self.proficiencies.add(skill)
+        
+    def add_class(self, cls: Class):
+        if not isinstance(cls, Class):
+            raise Exception('can only add Class objects')
+        self.classes.add(cls)
+    
+    def get_modifier(self, skill_or_attribute: str):
+        skill_or_attribute = skill_or_attribute.lower()
+        if (skill := getattr(self, skill_or_attribute, None)) is not None:
+            return (skill - 10) // 2
+        
+        attr = SKILL.get(skill_or_attribute)
+        base_mod = (getattr(self, attr) - 10) // 2
+        
+        prof_mod = 0 if skill_or_attribute not in self.proficiencies else self.proficiency
+        
+        return base_mod + prof_mod
+
+Owner = str
+CharacterName = str
+
+@dataclass
+class Rolodex():
+    owners: Dict[CharacterName, Set[Owner]] = field(default_factory=dict)
+    characters: Dict[CharacterName, Character] = field(default_factory=dict)
+
+    def __len__(self):
+        return len(self.characters)
+        
+    def add_character(self, owners: Union[Owner, List[Owner]], character: Character):
+        if character.name in self.characters:
+            raise Exception("character is already in rolodex!")
+        if len(owners) == 0:
+            raise Exception("someone must own the character")
+        if isinstance(owners, str):
+            owners = [owners]
+        self.characters[character.name.lower()] = character
+        self.owners[character.name.lower()] = owners
+        
+    def get_character(self, user: str, character_name: str):
+        character_name = character_name.lower()
+        if character_name not in self.characters:
+            raise ValueError("there is no character by this name")
+        if user not in self.owners[character_name]:
+            raise PermissionError("you cannot access this character")
+        return self.characters[character_name]
+    
+    def store(self):
+        with open('characters.txt', 'w') as fout: 
+            fout.write(str(self.__dict__))
+            
+    @staticmethod
+    def load():
+        with open('characters.txt', 'r') as fin:
+            rawdata = fin.read()
+        if rawdata:
+            data = eval(rawdata)
+            return Rolodex(**data)
+        else:
+            return Rolodex()
+
+def write_to_file(filename, data):
+    print('in func')
+
